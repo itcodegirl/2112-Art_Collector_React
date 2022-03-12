a76af55a962eab9d93dd87129bddb2c19f80ed45
itcodegirl@gmail.com <88835851+itcodegirl@users.noreply.github.com>
2022-03-11 18:43:20 -0600

    Add files via upload

---
+++ build/precache-manifest.e061fcc698e4c0a31d9976ff5ddc82ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75dafed841ee4e91bc3a30636e66304a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adb3a1cab9e6012fcc7",
+    "url": "/static/js/2.6c99d600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c99d600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc70b1aa42e1b02afea",
+    "url": "/static/js/main.a5ed05e3.chunk.js"
+  },
+  {
+    "revision": "aa7c612222590812d412",
+    "url": "/static/js/runtime-main.1160daa0.js"
+  }
+]);
